e83572e21fb7220baa6e68a132451da03982a429
itsbenny99 <acpilot70@gmail.com>
2020-02-20 00:59:11 +0000

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -183,7 +183,7 @@
 						@else
 						<div class="btn-group" role="group">
                             <button type="button" class="btn btn-rounded btn-dual-secondary" id="page-header-user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                {{Auth::user()->first_name}}<i class="fa fa-angle-down ml-5"></i>
+                                {{Auth::user()->fname}}<i class="fa fa-angle-down ml-5"></i>
                             </button>
                             <div class="dropdown-menu dropdown-menu-right min-width-150" aria-labelledby="page-header-user-dropdown">
                                 <a class="dropdown-item" href="/profile">
