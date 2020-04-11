497c33b52b4a6dee13dcf709798ed4918f8cab14
itsbenny99 <42702231+itsbenny99@users.noreply.github.com>
2020-04-11 15:46:21 +0100

    Update addnote.blade.php

--- resources/views/admin/mentors/addnote.blade.php
+++ resources/views/admin/mentors/addnote.blade.php
@@ -16,7 +16,11 @@
             <div class="col-sm-4">
                 <div class="form-group">
                 {!! Form::label('controller', 'Controller', ['class' => 'form-label']) !!}
+                    @if($c != null)
+                        {!! Form::select('controller', $controllers, $c, ['placeholder' => 'Select Controller', 'class' => 'form-control']) !!}
+                    @else
                         {!! Form::select('controller', $controllers, null, ['placeholder' => 'Select Controller', 'class' => 'form-control']) !!}
+                    @endif
                     
                 </div>
             </div>
